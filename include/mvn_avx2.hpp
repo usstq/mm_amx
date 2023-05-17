9bd1542eef9cd357f4f20887339abd456291afc8
Luo Cheng <cheng.luo@intel.com>
2023-05-17 06:06:28 +0300

    add mvn avx2 support

---
+++ include/mvn_avx2.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <cmath>
+
+#ifdef _WIN32
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#include <immintrin.h>
+#endif
+
+inline __m256i get_mask(int N7) {
+	static __m256i mask[] = {
+		_mm256_set_epi32( 0, 0, 0, 0, 0, 0, 0, 0),
+		_mm256_set_epi32( 0, 0, 0, 0, 0, 0, 0,-1),
+		_mm256_set_epi32( 0, 0, 0, 0, 0, 0,-1,-1),
+		_mm256_set_epi32( 0, 0, 0, 0, 0,-1,-1,-1),
+		_mm256_set_epi32( 0, 0, 0, 0,-1,-1,-1,-1),
+		_mm256_set_epi32( 0, 0, 0,-1,-1,-1,-1,-1),
+		_mm256_set_epi32( 0, 0,-1,-1,-1,-1,-1,-1),
+		_mm256_set_epi32( 0,-1,-1,-1,-1,-1,-1,-1),
+		_mm256_set_epi32(-1,-1,-1,-1,-1,-1,-1,-1),
+	};
+	return _mm256_loadu_si256(&mask[N7]);
+}
+
+// https://stackoverflow.com/questions/23189488/horizontal-sum-of-32-bit-floats-in-256-bit-avx-vector
+static inline float _mm256_reduce_add_ps(__m256 x) {
+    /* ( x3+x7, x2+x6, x1+x5, x0+x4 ) */
+    const __m128 x128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
+    /* ( -, -, x1+x3+x5+x7, x0+x2+x4+x6 ) */
+    const __m128 x64 = _mm_add_ps(x128, _mm_movehl_ps(x128, x128));
+    /* ( -, -, -, x0+x1+x2+x3+x4+x5+x6+x7 ) */
+    const __m128 x32 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));
+    /* Conversion to float is a no-op on x86-64 */
+    return _mm_cvtss_f32(x32);
+}
+
+static float sum(float* src, size_t ele_num) {
+    size_t i = 0;
+    __m256 s;
+    s = _mm256_xor_ps(s, s);
+    for (; i < ele_num / 8 * 8; i += 8) {
+        auto a0 = _mm256_loadu_ps(src);
+        s = _mm256_add_ps(s, a0);
+        src += 8;
+    }
+    if (i != ele_num) {
+        auto msk = get_mask(ele_num - i);
+        auto a0 = _mm256_maskload_ps(src, msk);
+        s = _mm256_add_ps(s, a0);
+    }
+    return _mm256_reduce_add_ps(s);
+}
+
+static float sum_power2(float* src, float mean, size_t ele_num) {
+    size_t i = 0;
+    __m256 s, zero;
+    s = _mm256_xor_ps(s, s);
+    zero = _mm256_xor_ps(zero, zero);
+    auto m = _mm256_set1_ps(mean);
+    for (; i < ele_num / 8 * 8; i += 8) {
+        auto a0 = _mm256_loadu_ps(src);
+        a0 = _mm256_sub_ps(a0, m);
+        s = _mm256_fmadd_ps(a0, a0, s);
+        src += 8;
+    }
+    if (i != ele_num) {
+        auto msk = get_mask(ele_num - i);
+        auto a0 = _mm256_maskload_ps(src, msk);
+        a0 = _mm256_sub_ps(a0, m);
+        a0 = _mm256_blendv_ps(zero, a0, _mm256_castsi256_ps(msk));
+        s = _mm256_fmadd_ps(a0, a0, s);
+    }
+    return _mm256_reduce_add_ps(s);
+}
+
+static void mvn(float* src, float mean, float var, size_t ele_num, float* dst) {
+    size_t i = 0;
+    auto m = _mm256_set1_ps(mean);
+    auto v = _mm256_set1_ps(var);
+    for (; i < ele_num / 8 * 8; i += 8) {
+        auto a0_f = _mm256_loadu_ps(src);
+        a0_f = _mm256_sub_ps(a0_f, m);
+        a0_f = _mm256_mul_ps(a0_f, v);
+        _mm256_storeu_si256(reinterpret_cast<__m256i *>(dst), (__m256i)a0_f);
+
+        src += 8;
+        dst += 8;
+    }
+    if (i != ele_num) {
+        auto msk = get_mask(ele_num - i);
+        auto a0_f = _mm256_maskload_ps(src, msk);
+        a0_f = _mm256_sub_ps(a0_f, m);
+        a0_f = _mm256_mul_ps(a0_f, v);
+        _mm256_maskstore_ps(dst, msk, a0_f);
+    }
+}
+
+inline void mvn_line(float* src, size_t ele_num, float eps, bool inside_sqrt, float *dst) {
+    // mean
+    float mean = sum(src, ele_num) / ele_num;
+    // var
+    float var = sum_power2(src, mean, ele_num) / ele_num;
+    var = 1.0f / (inside_sqrt ? std::sqrt(var + eps) : std::sqrt(var) + eps);
+    // mvn
+    mvn(src, mean, var, ele_num, dst);
+}
---
+++ test_mvn.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include <cstring>
+#include <thread>
+#include <cmath>
+
+#include "misc.hpp"
+#include "mvn_avx2.hpp"
+#include "tensor2D.hpp"
+#include "timeit.hpp"
+#include <omp.h>
+#include "test_bw.hpp"
+// https://raw.githubusercontent.com/intel/perfmon/main/SPR/events/sapphirerapids_core.json
+timeit benchmark
+(
+    {
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES, "HW_CYCLES"},
+        //{PERF_TYPE_RAW, 0x3c, "CPU_CLK_UNHALTED.THREAD"},
+        //{PERF_TYPE_RAW, 0x81d0, "MEM_LOAD_RETIRED.ALL_LOADS"},
+        //{PERF_TYPE_HW_CACHE, 0x10002, "LLC_load_misses"},
+        //{PERF_TYPE_HW_CACHE, 0x2, "LLC_loads"},
+        //{PERF_TYPE_RAW, 0x02b1, "UOPS_EXECUTED.CORE"},
+    }
+);
+
+
+// vfmadd132ps ymm(8 floats)  Throughput (CPI)=0.5
+const double vfmaddOpsPerCycle = 16;
+
+
+int OMP_NT = omp_thread_count();
+
+int test_mvn() {
+    tensor2D<float> x;
+    tensor2D<float> y0;
+    tensor2D<float> y1;
+    float eps = 1e-5;
+    bool inside_sqrt = true;
+
+    auto mvn_ref = [&](tensor2D<float>& x, tensor2D<float>& y) {
+        float x_max = std::numeric_limits<float>::lowest();
+        float sum = 0;
+        auto ele_num = x.dims[1];
+        for(int i = 0; i < x.dims[1]; i++) {
+            sum += x[i];
+        }
+        float mean = sum / ele_num;
+        float sum_power2 = 0;
+        for(int i = 0; i < x.dims[1]; i++) {
+            sum_power2 += (x[i] - mean) * (x[i] - mean);
+        }
+        float var = sum_power2 / ele_num;
+        var = 1.0f / (inside_sqrt ? std::sqrt(var + eps) : std::sqrt(var) + eps);
+        for(int i = 0; i < x.dims[1]; i++) {
+            y[i] = (x[i] - mean) * var;
+        }
+    };
+    int errors = 0;
+    for(int N = 1; N < 129; N++) {
+        x.resize(1, N);
+        x.fill_rnd();
+        y0 = x.clone();
+        y1 = x.clone();
+        mvn_ref(x, y0);
+        mvn_line(&x[0], N, eps, inside_sqrt, &y1[0]);
+        for(int i=0;i<N;i++) {
+            if (abs((y0[i] - y1[i])/y0[i]) > 0.0001f) {
+                errors ++;
+                std::cout << "#" << i << "/" << N << ":  " <<y0[i] << " vs " << y1[i] << " diff " << (y0[i] - y1[i]) << std::endl;
+            }
+        }
+    }
+    if (errors == 0) {
+        std::cout << ANSIcolor("32") << __func__ << " Pass" << ANSIcolor() << std::endl;
+    }
+    return 0;
+}
+
+int main(int argc, const char *argv[]) {
+    benchmark.set_app(argv[0]);
+
+    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+    std::cout << ANSIcolor("31") << "omp_get_num_threads() = " << omp_get_num_threads() << std::endl << ANSIcolor();
+    std::cout << ANSIcolor("31") << "OMP_NT = " << OMP_NT << std::endl << ANSIcolor();
+
+    //return test_vmaskmovps_alignment();
+    //return test_exp();
+    //return test_hmax();
+    //return test_softmax();
+    //test_all_bw(3);
+    test_mvn();
+
+    return 0;
+}
