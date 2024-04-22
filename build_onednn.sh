4b849c26e7aefb67c284ed1713bf2d7be176fad1
Li, Tingqian <tingqian.li@intel.com>
2024-04-21 21:01:00 -0700

    update

--- build_onednn.sh
+++ build_onednn.sh
@@ -1,6 +1,6 @@
-
+#!/bash/bin
 
 mkdir -p thirdparty/oneDNN/build
 cd thirdparty/oneDNN/build
-cmake -DCMAKE_INSTALL_PREFIX=./install .
-cmake --build . --target install
+cmake -DCMAKE_INSTALL_PREFIX=./install ..
+cmake --build . --target install -- -j 24
--- test-hw-prefetcher.cpp
+++ test-hw-prefetcher.cpp
@@ -45,6 +45,35 @@ timeit timer({
 });
 
 
+class MemAccessPattern : public jit_generator {
+ public:
+  std::vector<int> m_cache_lines;
+  bool m_is_write;
+  MemAccessPattern(const std::vector<int>& cache_lines, bool is_write) : m_cache_lines(cache_lines), m_is_write(is_write) {
+    create_kernel("MemAccessPattern");
+  }
+  // to save push/pop: do not use `abi_save_gpr_regs`
+  Xbyak::Reg64 reg_addr = abi_param1;
+  void generate() {
+    mfence();
+    if (!m_is_write) {
+        vpxorq(zmm0, zmm0, zmm0);
+        for(auto& i : m_cache_lines) {
+            vmovups(zmm1, ptr[reg_addr + i*64]);
+            vaddps(zmm0, zmm0, zmm1);
+        }
+    } else {
+        // write
+        vpxorq(zmm0, zmm0, zmm0);
+        for(auto& i : m_cache_lines) {
+            vmovups(ptr[reg_addr + i*64], zmm0);
+        }
+    }
+    mfence();
+    ret();
+  }
+};
+
 class MeasureAccess : public jit_generator {
  public:
   
@@ -85,23 +114,27 @@ class MeasureAccess : public jit_generator {
 };
 
 void test_read_prefetch() {
-    MeasureAccess measure_access;
 
+    EnvVar WRMEM("WRMEM", 0);
+    EnvVar CLSTRIDE("CLSTRIDE", 1);
+
+    std::vector<int> read_pattern;
+    for(int i = 0; i < 5; i++) {
+        read_pattern.push_back(i*(int)CLSTRIDE);
+    }
+    //std::vector<int> read_pattern = {0, 4, 4*2};    // +3
+    //std::vector<int> read_pattern = {0, 4};       // +2~3
+    MeasureAccess measure_access;
+    MemAccessPattern mem_reads_writes(read_pattern, WRMEM);
     MSRConfig _msr1(0x1A4, MASK_1A4_PF);
+
+    std::vector<uint8_t> big_buffer(1024*1024*128, 0);
 
     uint64_t nbytes = 8192;
     auto* data = reinterpret_cast<uint8_t*>(aligned_alloc(4096, nbytes));
     for(int i = 0; i < nbytes; i++) data[i] = 1;
 
-    auto data_access = [&]() {
-        auto sum = _mm512_setzero_ps();
-        for(int i = 6; i < 16; i++) sum += _mm512_loadu_ps(data + i*64);
-        if(_mm512_reduce_add_ps(sum) == 1) {
-            abort();
-        }
-    };
-
-    auto wait_tsc = second2tsc(1e-3);
+    auto wait_tsc = second2tsc(1e-5);
     auto wait = [&](){
         auto t0 = __rdtsc();
         while(__rdtsc() - t0 < wait_tsc);
@@ -110,22 +143,27 @@ void test_read_prefetch() {
     // 8192 = 128 x 64 = 128 cacheline
     std::vector<int64_t> access_times(nbytes/64, 0);
 
-    const int repeats = 1;
+    const int repeats = 10;
 
     for(int r = 0; r < repeats; r ++) {
         for(int cache_line = 0; cache_line < access_times.size(); cache_line++) {
             // flush the probing array
             // clflush(data, nbytes);
             {
-                for (int i = 0; i < nbytes; i += 64) _mm_clflush(data + i);
+                for (int i = 0; i < nbytes; i += 64) {
+                    _mm_clflush(data + i);
+                    _mm_mfence();
+                }
+
+                // mflush prevent HW prefetcher to work for some reason, need memset
+                memset(&big_buffer[0], cache_line, big_buffer.size());
+                load_prefetch_L2(&big_buffer[0], big_buffer.size());
                 _mm_mfence();
             }
 
-            wait();
+            //wait();
 
-            // access pattern triggers HW prefetch
-            data_access();
-            _mm_mfence();
+            mem_reads_writes(data);
 
             wait();
 
@@ -137,11 +175,17 @@ void test_read_prefetch() {
 
     // show access_times
     for(int cache_line = 0; cache_line < access_times.size(); cache_line++) {
-        
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
+        const char * fg_color = "90";
+        if (std::find(read_pattern.begin(), read_pattern.end(), cache_line) != read_pattern.end()) {
+            fg_color = "32";
+        }
+
         auto nbars = static_cast<int>(tsc2second(access_times[cache_line]/repeats)*1e9 * 100/256);
         std::string progress_bar;
-        for(int i = 0; i < nbars; i++) progress_bar += "█";
-        printf(" cache_line[%3d] : %6.2f ns : \033[1;90m %s \033[0m\n", cache_line, tsc2second(access_times[cache_line]/repeats)*1e9, progress_bar.c_str());
+        // https://github.com/Changaco/unicode-progress-bars/blob/master/generator.html
+        for(int i = 0; i < nbars; i++) progress_bar += "▅";// "█";
+        printf(" cache_line[%3d] : %6.2f ns : \033[1;%sm %s \033[0m\n", cache_line, tsc2second(access_times[cache_line]/repeats)*1e9, fg_color, progress_bar.c_str());
     }
 }
 
